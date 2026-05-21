7c5f99d52d33711362dcd443d7bf2174917b45de
Moke Patrick Armel <mokepatrickarmel@gmail.com>
2026-05-21 05:18:42 +0000

    fix: set official public anon Supabase key in config file

--- supabase-config.js
+++ supabase-config.js
@@ -5,7 +5,7 @@
 // Projet Settings -> API -> Project URL & Project API Keys (anon public)
 
 const SUPABASE_URL = "https://vbvffkrgzmahdghwbvlg.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_egWUtDTzJK2X6cOlOKVtUw_UR8scIOW";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZidmZma3Jnem1haGRnaHdidmxnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkyODY3MzAsImV4cCI6MjA5NDg2MjczMH0.wvDG6eye9CHkocDXqtFNrUbCpLuUPG6NagsVn4vUrW0";
 
 // Initialisation du client Supabase
 let supabase = null;
